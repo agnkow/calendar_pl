bf25c17bffe4488bdd18ecc8146a12c8922ad79e
Agnieszka Kowalik <35640119+agnkow@users.noreply.github.com>
2019-01-27 22:06:52 +0100

    Add files via upload

--- get_calendar_pl.py
+++ get_calendar_pl.py
@@ -62,11 +62,17 @@ def easter_date(year):
     a = year % 19
     b = year // 100
     c = year % 100
-    d = (19 * a + b - b // 4 - ((b - (b + 8) // 25 + 1) // 3) + 15) % 30
-    e = (32 + 2 * (b % 4) + 2 * (c // 4) - d - (c % 4)) % 7
-    f = d + e - 7 * ((a + 11 * d + 22 * e) // 451) + 114
-    month = f // 31
-    day = f % 31 + 1   
+    d = b // 4
+    e = b % 4
+    f = (b + 8) // 25
+    g = (b - f + 1) // 3
+    h = (19*a + b - d - g + 15) % 30
+    i = c // 4
+    k = c % 4
+    l = (32 + 2*e + 2*i - h - k) % 7
+    m = (a + 11*h + 22*l) // 451
+    month = (h + l - 7*m + 114) // 31
+    day = ((h + l - 7*m + 114) % 31) + 1  
     return date(year, month, day)
 
 	
